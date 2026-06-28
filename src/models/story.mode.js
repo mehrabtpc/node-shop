764c122aaaf57a1f11de4ad383c0e10b66e6636d
Mehrab Tajik <mehrabt.pc@gmail.com>
2026-06-28 19:36:39 +0330

    develop some controllers

--- src/models/story.mode.js
+++ src/models/story.mode.js
@@ -4,8 +4,8 @@ const storySchema = new mongoose.Schema(
   {
     title: { type: String, required: true, trim: true },
     image: { type: String, required: true },
-    link: { type: String, default: null }, // لینک مستقیم به محصول داخل استوری
-    expires_at: { type: Date, default: null }, // تاریخ انقضای استوری
+    link: { type: String, default: null },
+    expires_at: { type: Date, default: null },
     status: { type: String, enum: ["active", "inactive"], default: "active", index: true }
   },
   { timestamps: true }
